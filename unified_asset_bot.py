20c2ae6ad7b93491d244ae451f74a47cda616d62
Yasir Abed Rabbu <yasirabedrabbu@gmail.com>
2026-08-29 09:51:13 +0600

    Add main guard to unified_asset_bot.py

--- unified_asset_bot.py
+++ unified_asset_bot.py
@@ -329,3 +329,7 @@ def main() -> None:
     else:
         logger.info("Running in polling mode")
         application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()

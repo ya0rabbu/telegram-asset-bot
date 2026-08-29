d893097f7f36a39d330ddbd690dd3686fc042e11
Yasir Abed Rabbu <yasirabedrabbu@gmail.com>
2026-08-29 10:01:02 +0600

    Fix indentation for webhook_url check

--- unified_asset_bot.py
+++ unified_asset_bot.py
@@ -318,7 +318,7 @@ def main() -> None:
 
     logger.info("Unified Lummi/Hugeicons bot is starting")
 
-        if webhook_url:
+    if webhook_url:
         logger.info("Running in webhook mode on port %s", port)
         application.run_webhook(
             listen="0.0.0.0",
